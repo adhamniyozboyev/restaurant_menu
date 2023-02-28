4fdd280849bfe86cad1ca909a4025f994bf7b8f9
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-28 09:25:09 +0500

    start

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          ListTile(
+            title: Text('$index-pupil',style: TextStyle(fontSize: 20,color: Colors.red),),
+          );
+        },
+      ),
+    );
+  }
+}
