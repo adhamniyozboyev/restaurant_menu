a9c4f5dceca8dc80dc1b6685978c32ca224a3d83
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-28 10:28:30 +0500

    items count

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
@@ -17,11 +18,30 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Items'))),
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: 15,
         itemBuilder: (context, index) {
-          ListTile(
-            title: Text('$index-pupil',style: TextStyle(fontSize: 20,color: Colors.red),),
+          return Card(
+            shadowColor: Colors.red,
+            shape: Border(
+                left: BorderSide(style: BorderStyle.solid, width: 3.5),
+                bottom: BorderSide(style: BorderStyle.solid, width: 3.5),
+                right: BorderSide(style: BorderStyle.solid, width: 3.5),
+                top: BorderSide(style: BorderStyle.solid, width: 3.5)),
+            elevation: 40,
+            child: ListTile(
+              iconColor: Colors.black,
+              textColor: Colors.green,
+              trailing: Icon(Icons.arrow_forward_ios),
+              subtitle: Center(child: Text('subtitle${index + 1}')),
+              leading: CircleAvatar(child: Icon(Icons.person)),
+              title: Center(
+                child: Text(
+                  'Item # ${index + 1}',
+                ),
+              ),
+            ),
           );
         },
       ),
